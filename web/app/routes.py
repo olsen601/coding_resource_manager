ce63c148712505b2540ef08a0ad2cf254f0bc891
olsen601 <olsen601@d.umn.edu>
2018-03-18 23:23:52 -0500

    Added api request cache and altered display of returned data as a list of titles hyperlinked to associated urls.

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
+from api import stack_api
 from app import app
-import requests
 
 @app.route('/')
 @app.route('/index')
@@ -9,14 +9,12 @@ def index():
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
-    response = ''
+    global response
     if request.method == 'POST':
         term = str(request.form.get('search_input')) # as a default use 'mock', later I will add a second search input for tags
-        url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&closed=False&tagged=python-3.x&title="+term+"&site=stackoverflow"
-        response = requests.get(url)
-        r = app.response_class(response.content, content_type='application/json')
-        return r #need to impliment a cache and seperate the get/post to allow data to be formated before it is returned.
-    return render_template('results.html', title='Results', body=r)
+        response = stack_api.get_stack_source(term)
+        return render_template('results.html', title='Results', body=response)
+    return render_template('results.html', title='Results', body=response)
 
 @app.route('/favorites')
 def favorites():
