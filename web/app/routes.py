91306329ffc99346ead132dce60bd2e028560db7
olsen601 <tedssmith2556@gmail.com>
2018-03-20 00:44:20 -0500

    Added bookmarking and persistent store feature, also added a README.md

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from api import stack_api
+from data_store import archive
 from app import app
 
 @app.route('/')
@@ -11,14 +12,24 @@ def index():
 def results():
     global response
     if request.method == 'POST':
-        term = str(request.form.get('search_input')) # as a default use 'mock', later I will add a second search input for tags
+        term = str(request.form.get('search_input'))
         response = stack_api.get_stack_source(term)
         return render_template('results.html', title='Results', body=response)
     return render_template('results.html', title='Results', body=response)
 
-@app.route('/favorites')
-def favorites():
-    return render_template('favorites.html', title='Favorites')
+@app.route('/bookmark', methods=['GET', 'POST'])
+def bookmark():
+    archive.db_setup()
+    global bookmarks
+    if request.method == 'POST':
+        source = str(request.form.get('source'))
+        title = str(request.form.get('title'))
+        link = str(request.form.get('link'))
+        archive.db_add(source, title, link)
+        bookmarks = archive.db_data()
+        return render_template('bookmark.html', title='Bookmarks', body=bookmarks)
+    bookmarks = archive.db_data()
+    return render_template('bookmark.html', title='Bookmarks', body=bookmarks)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, threaded=True, debug=True)
